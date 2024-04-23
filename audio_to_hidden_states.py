718079ec599fda461813e1e980a0ff9edd3cf245
martijnbentum <bentummartijn@gmail.com>
2024-04-23 17:53:03 +0200

    module to convert audio file to wav2vec hidden states

---
+++ audio_to_hidden_states.py
@@ -0,0 +1,35 @@
+import librosa
+import locations
+import torch
+from transformers import Wav2Vec2Processor
+from transformers import Wav2Vec2Model
+
+cache_directory = '/vol/tensusers/mbentum/CGN_AUDIO_EXTRACT/WAV2VEC_DATA'
+default_checkpoint = 'facebook/wav2vec2-xls-r-300m'
+
+def load_audio(filename, start = 0.0, end=None):
+	if not end: duration = None
+	else: duration = end - start
+	audio, sr = librosa.load(filename, sr = 16000, offset=start, 
+        duration=duration)
+	return audio
+
+def load_pretrained_processor_model(checkpoint = None, cache_directory = None):
+    if not checkpoint: checkpoint = default_checkpoint
+    if not cache_directory: cache_directory = cache_dir
+    processor = Wav2Vec2Processor.from_pretrained(checkpoint)
+    model = Wav2Vec2Model.from_pretrained(name,
+        cache_dir = cache_directory)
+    return processor, model
+
+def audio_to_pretrained_outputs(audio_filename, start=None, end=None, 
+    processor=None, model=None, frame_duration = None):
+    if not processor or not model: 
+        print('loading pretrained model: facebook/wav2vec2-xls-r-2b')
+        processor, model = load_pretrained_processor_model()
+    array = audio.load_audio(audio_filename,start, end)
+    inputs = processor(array, sampling_rate=16_000, return_tensors='pt',
+        padding= True)
+    with torch.no_grad():
+        outputs = model(**inputs,output_hidden_states = True)
+    return outputs
