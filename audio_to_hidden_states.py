8a0ea488d05b6f18a597459d4c36d9e666fc2763
martijnbentum <bentummartijn@gmail.com>
2024-04-24 17:08:29 +0200

    working version 1

--- audio_to_hidden_states.py
+++ audio_to_hidden_states.py
@@ -1,10 +1,10 @@
 import librosa
-import locations
 import torch
+from transformers import Wav2Vec2FeatureExtractor
 from transformers import Wav2Vec2Processor
 from transformers import Wav2Vec2Model
 
-default_cache_directory='/vol/tensusers/mbentum/CGN_AUDIO_EXTRACT/WAV2VEC_DATA/'
+default_cache_directory='/vol/tensusers/mbentum/hidden_states/cache'
 default_checkpoint = 'facebook/wav2vec2-xls-r-300m'
 
 def load_audio(filename, start = 0.0, end=None):
@@ -14,24 +14,27 @@ def load_audio(filename, start = 0.0, end=None):
         duration=duration)
 	return audio
 
-def load_pretrained_processor_model(checkpoint = None, cache_directory = None):
+def make_feature_extractor():
+    feature_extractor = Wav2Vec2FeatureExtractor(feature_size=1, 
+    sampling_rate=16000, padding_value=0.0, do_normalize=True, 
+    return_attention_mask=True)
+    return feature_extractor
+
+def load_pretrained_model(checkpoint = None, cache_directory = None):
     if not checkpoint: checkpoint = default_checkpoint
     if not cache_directory: cache_directory = default_cache_directory
-    if checkpoint == default_checkpoint:
-        processor_checkpoint = '../checkpoint'
-    else: processor_checkpoint = checkpoint
-    processor = Wav2Vec2Processor.from_pretrained(processor_checkpoint)
     model = Wav2Vec2Model.from_pretrained(checkpoint,
         cache_dir = cache_directory)
-    return processor, model
+    return model
 
 def audio_to_pretrained_outputs(audio_filename, start=None, end=None, 
-    processor=None, model=None, frame_duration = None):
-    if not processor or not model: 
-        print('loading pretrained model: facebook/wav2vec2-xls-r-2b')
-        processor, model = load_pretrained_processor_model()
-    array = audio.load_audio(audio_filename,start, end)
-    inputs = processor(array, sampling_rate=16_000, return_tensors='pt',
+    feature_extractor = None, model=None, frame_duration = None):
+    if not model: 
+        model = load_pretrained_model()
+    if not feature_extractor:
+        feature_extractor = make_feature_extractor()
+    array = load_audio(audio_filename,start, end)
+    inputs = feature_extractor(array, sampling_rate=16_000, return_tensors='pt',
         padding= True)
     with torch.no_grad():
         outputs = model(**inputs,output_hidden_states = True)
