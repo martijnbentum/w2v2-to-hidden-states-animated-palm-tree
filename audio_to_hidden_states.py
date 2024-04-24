2231f84a1ff3a3eac90f0dc8bdd9ce0829a5557e
martijnbentum <bentummartijn@gmail.com>
2024-04-24 12:08:56 +0200

    loading processor and model maybe not working correctly

--- audio_to_hidden_states.py
+++ audio_to_hidden_states.py
@@ -4,7 +4,7 @@ import torch
 from transformers import Wav2Vec2Processor
 from transformers import Wav2Vec2Model
 
-cache_directory = '/vol/tensusers/mbentum/CGN_AUDIO_EXTRACT/WAV2VEC_DATA'
+default_cache_directory='/vol/tensusers/mbentum/CGN_AUDIO_EXTRACT/WAV2VEC_DATA/'
 default_checkpoint = 'facebook/wav2vec2-xls-r-300m'
 
 def load_audio(filename, start = 0.0, end=None):
@@ -16,9 +16,12 @@ def load_audio(filename, start = 0.0, end=None):
 
 def load_pretrained_processor_model(checkpoint = None, cache_directory = None):
     if not checkpoint: checkpoint = default_checkpoint
-    if not cache_directory: cache_directory = cache_dir
-    processor = Wav2Vec2Processor.from_pretrained(checkpoint)
-    model = Wav2Vec2Model.from_pretrained(name,
+    if not cache_directory: cache_directory = default_cache_directory
+    if checkpoint == default_checkpoint:
+        processor_checkpoint = '../checkpoint'
+    else: processor_checkpoint = checkpoint
+    processor = Wav2Vec2Processor.from_pretrained(processor_checkpoint)
+    model = Wav2Vec2Model.from_pretrained(checkpoint,
         cache_dir = cache_directory)
     return processor, model
 
