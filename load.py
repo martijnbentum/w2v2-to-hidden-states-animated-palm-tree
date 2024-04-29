500142f50b40845d8f117f6eba384a0f7d683182
martijnbentum <bentummartijn@gmail.com>
2024-04-29 18:16:03 +0200

    load contains all loading functionality load model load audio

---
+++ load.py
@@ -0,0 +1,30 @@
+import librosa
+import torch
+from transformers import Wav2Vec2FeatureExtractor
+from transformers import Wav2Vec2Processor
+from transformers import Wav2Vec2Model
+
+default_cache_directory='/vol/tensusers/mbentum/hidden_states/cache'
+default_checkpoint = 'facebook/wav2vec2-xls-r-300m'
+
+def load_audio(filename, start = 0.0, end=None):
+	if not end: duration = None
+	else: duration = end - start
+	audio, sr = librosa.load(filename, sr = 16000, offset=start, 
+        duration=duration)
+	return audio
+
+def make_feature_extractor():
+    feature_extractor = Wav2Vec2FeatureExtractor(feature_size=1, 
+        sampling_rate=16000, padding_value=0.0, do_normalize=True, 
+        return_attention_mask=True)
+    return feature_extractor
+
+def load_pretrained_model(checkpoint = None, cache_directory = None, 
+        gpu = False):
+    if not checkpoint: checkpoint = default_checkpoint
+    if not cache_directory: cache_directory = default_cache_directory
+    model = Wav2Vec2Model.from_pretrained(checkpoint,
+        cache_dir = cache_directory)
+    if gpu: model.to('cuda')
+    return model
