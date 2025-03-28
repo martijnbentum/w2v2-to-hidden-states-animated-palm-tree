fef326da152be2fe6ac82e5af1f5eacb0906d19f
martijnbentum <bentummartijn@gmail.com>
2025-03-28 12:22:55 +0100

    new build

--- build/lib/w2v2_hidden_states/load.py
+++ build/lib/w2v2_hidden_states/load.py
@@ -5,6 +5,7 @@ from transformers import Wav2Vec2FeatureExtractor
 from transformers import Wav2Vec2Processor
 from transformers import Wav2Vec2Model
 from transformers import Wav2Vec2ForPreTraining as pt
+from transformers import AutoModel
 
 default_cache_directory='/vol/tensusers/mbentum/hidden_states/cache'
 if not os.path.exists(default_cache_directory):
@@ -14,6 +15,7 @@ if not os.path.exists(default_cache_directory):
 pretrained_small = 'facebook/wav2vec2-xls-r-300m'
 pretrained_big = 'facebook/wav2vec2-xls-r-2b'
 default_checkpoint = 'facebook/wav2vec2-xls-r-300m'
+hubert_base = 'facebook/hubert-base-ls960'
 
 def load_audio(filename, start = 0.0, end=None):
 	if not end: duration = None
@@ -32,7 +34,8 @@ def load_pretrained_model(checkpoint = None, cache_directory = None,
         gpu = False):
     if not checkpoint: checkpoint = default_checkpoint
     if not cache_directory: cache_directory = default_cache_directory
-    model = Wav2Vec2Model.from_pretrained(checkpoint,
+    # model = Wav2Vec2Model.from_pretrained(checkpoint,
+    model = AutoModel.from_pretrained(checkpoint,
         cache_dir = cache_directory)
     if gpu: model.to('cuda')
     return model
@@ -42,3 +45,7 @@ def load_model_pt(checkpoint = None, gpu = False):
     model_pt = pt.from_pretrained(default_checkpoint)
     if gpu: model_pt.to('cuda')
     return model_pt
+
+def load_hubert_base_model(cache_directory = None, gpu = False):
+    return load_pretrained_model(hubert_base, cache_directory, gpu)
+
